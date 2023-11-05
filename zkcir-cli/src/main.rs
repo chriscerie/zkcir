9d95483a3a329befa70bc06202b9a6c74c790da4
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-11-05 09:21:55 -0800

    Replace framework with forked framework from cli

--- zkcir-cli/src/main.rs
+++ zkcir-cli/src/main.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::{
     env::{self},
@@ -13,13 +14,91 @@ use args::get_args;
 
 mod args;
 
+#[derive(Debug)]
+enum TargetFramework {
+    Plonky2,
+}
+
+impl fmt::Display for TargetFramework {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TargetFramework::Plonky2 => write!(f, "plonky2"),
+        }
+    }
+}
+
+impl TargetFramework {
+    pub fn replace_deps(&self, path: &Path, pb: &ProgressBar) {
+        match self {
+            TargetFramework::Plonky2 => {
+                let git_url = "https://github.com/chriscerie/plonky2.git".to_string();
+                let target_packages = [
+                    "plonky2",
+                    "plonky2_evm",
+                    "plonky2_field",
+                    "plonky2_maybe_rayon",
+                    "starky",
+                    "util",
+                ];
+
+                pb.inc_length(target_packages.len() as u64);
+
+                for package in target_packages {
+                    pb.set_message(format!(": {package}"));
+                    Command::new("cargo")
+                        .args(["add", package])
+                        .args(["--git", &git_url])
+                        .current_dir(path)
+                        .output()
+                        .expect("Failed to execute `cargo add`");
+
+                    pb.println(format!(
+                        "{} dependency ({package})",
+                        get_formatted_left_output_green("Replaced")
+                    ));
+                    pb.inc(1);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let current_dir = env::current_dir().expect("Failed to get current directory");
 
     let args = get_args();
 
-    let pb = get_new_pb(3_u64, "Running", ": cargo");
+    let pb = get_new_pb(3_u64, "Running");
 
+    pb.set_message(": cargo");
+    let parsed_cargo = get_parsed_cargo();
+
+    let dependencies = parsed_cargo
+        .get("dependencies")
+        .expect("No dependencies found in `Cargo.toml`");
+
+    pb.println(format!(
+        "{} {}",
+        get_formatted_left_output_green("Found"),
+        current_dir.join("Cargo.toml").display()
+    ));
+
+    pb.set_message(": target framework");
+    let target_framework;
+    if dependencies.get("plonky2").is_some() {
+        target_framework = TargetFramework::Plonky2;
+    } else {
+        panic!("No supported target framework found");
+    }
+
+    pb.println(format!(
+        "{} target framework ({})",
+        get_formatted_left_output_green("Found"),
+        target_framework.to_string()
+    ));
+    pb.inc(1);
+
+    pb.set_message(": temp dir");
     let temp_dir = tempdir().expect("Failed to create temp dir");
 
     pb.println(format!(
@@ -29,6 +108,7 @@ fn main() {
     ));
     pb.inc(1);
 
+    pb.set_message(": copy contents");
     let num_copied_files =
         copy_to(&current_dir, temp_dir.path()).expect("Failed to copy contents to temp dir");
 
@@ -38,12 +118,43 @@ fn main() {
     ));
     pb.inc(1);
 
+    // If the root temp dir contains a workspace `Cargo.toml`, cargo would fail to build unless there is a
+    // `[workspace]` in this `Cargo.toml`
+    if parsed_cargo.get("workspace").is_none() {
+        pb.inc_length(1);
+        pb.set_message(": add [workspace]");
+
+        let mut toml_string =
+            toml::to_string(&parsed_cargo).expect("Failed to serialize `Cargo.toml`");
+
+        let prepend_chars = "[workspace]\n";
+        toml_string.insert_str(0, prepend_chars);
+
+        fs::write(temp_dir.path().join("Cargo.toml"), toml_string)
+            .expect("Failed to add `[workspace]` to `Cargo.toml");
+
+        pb.println(format!(
+            "{} empty `[workspace]` ({})",
+            get_formatted_left_output_green("Added"),
+            temp_dir
+                .path()
+                .join("Cargo.toml")
+                .to_str()
+                .expect("Failed to get `Cargo.toml`")
+        ));
+        pb.inc(1);
+    }
+
+    target_framework.replace_deps(&temp_dir.path(), &pb);
+
     if let Some(example) = args.example {
+        pb.inc_length(1);
+
+        pb.set_message(format!(": run"));
         let output = Command::new("cargo")
             .arg("run")
-            .arg("--example")
-            .arg(&example)
-            .current_dir(current_dir) // Set the current directory for the command
+            .args(["--example", &example])
+            .current_dir(temp_dir.path())
             .output()
             .expect("Failed to execute `cargo run`");
 
@@ -60,10 +171,23 @@ fn main() {
                     .unwrap(),
             );
             pb.finish_with_message(get_formatted_left_output_green("Emitted"));
+            println!("{}", String::from_utf8_lossy(&output.stdout));
         } else {
-            println!("cargo run failed");
+            println!(
+                "cargo run failed: {}",
+                String::from_utf8_lossy(&output.stderr)
+            );
         }
     }
+}
+
+fn get_parsed_cargo() -> toml::Value {
+    let contents = fs::read_to_string("Cargo.toml").expect("Could not find `Cargo.toml`");
+
+    contents
+        .parse::<toml::Value>()
+        .expect("Failed to parse `Cargo.toml`")
+        .to_owned()
 }
 
 fn copy_to(from: &Path, to: &Path) -> io::Result<u64> {
@@ -107,16 +231,12 @@ fn get_formatted_left_output_blue(output: &str) -> String {
     format!("{blue_bold}{:>12}{reset}", output)
 }
 
-fn get_new_pb(
-    length: u64,
-    progress_message_left: &str,
-    progress_message_right: &str,
-) -> ProgressBar {
+fn get_new_pb(length: u64, progress_message_left: &str) -> ProgressBar {
     let pb = ProgressBar::new(length);
     pb.set_style(
         ProgressStyle::default_bar()
             .template(&format!(
-                "{} [{{bar:40}}] {{pos}}/{{len}}{progress_message_right}",
+                "{} [{{bar:40}}] {{pos}}/{{len}}{{msg}}",
                 get_formatted_left_output_blue(progress_message_left)
             ))
             .unwrap()
