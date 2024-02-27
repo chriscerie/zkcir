5220d6764f02abad9c909c86e080a33f1e0add49
Maya Ma <yema@cs.ucsb.edu>
2024-02-27 00:14:07 -0800

    added public/constant to wire

--- zkcir/src/ast/expr.rs
+++ zkcir/src/ast/expr.rs
@@ -280,6 +280,8 @@ pub struct Wire {
     pub row: usize,
     pub column: usize,
     pub value: Option<Value>,
+    pub is_constant: bool,
+    pub is_public: bool, 
 }
 
 impl Wire {
@@ -289,6 +291,8 @@ impl Wire {
             row,
             column,
             value: None,
+            is_constant: true,
+            is_public: true,
         }
     }
 }
@@ -373,6 +377,8 @@ mod tests {
                             row: 1,
                             column: 2,
                             value: Some(Value::U64(5)),
+                            is_constant: true, //initialize with proper value
+                            is_public: true, //initialize with proper value
                         }
                         .into(),
                     ),
